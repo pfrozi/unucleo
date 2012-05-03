e34eb2361e30b36512e3a29c455e34a9fa87e193
fongoses@gmail.com <fongoses@gmail.com@3ab7fcc2-692c-f5df-6918-9b788a5e821a>
2012-05-03 00:21:10 +0000

    Mais comentários e colocção da função scheduler()

--- trunk/unucleo.c
+++ trunk/unucleo.c
@@ -78,6 +78,8 @@ void ini_make(ucontext_t* contexto) {
 }
 /* Funções reservadas do sistema operacional - FIM */
 
+/* Inicializa o unucleo.
+   Retorna "0" se inicializou corretamente ou "-1" caso contrário. */
 int libsisop_init(void) {
     PCB init_pcb;
     int i;
@@ -107,6 +109,10 @@ int libsisop_init(void) {
 	return init_pcb.pid;
 }
 
+/* Cria um novo processo de usuário com a prioridade "prio".
+   A prioridade deve ser 1 ou 2 senão a função retorna erro.
+   O código a ser executado é apontado por "start_routine".
+   Retorna o PID (Process Identification) do processo ou "-1" em caso de erro. */
 int mproc_create(int prio, void *(*start_routine)(void*), void *arg) {
     PCB pcb_add;
 
@@ -130,6 +136,21 @@ int mproc_create(int prio, void *(*start_routine)(void*), void *arg) {
 	return pcb_add.pid;
 }
 
+/* O processo retorna o controle ao escalonador (cedência voluntária)
+   e vai para o final da fila de APTOS para executar. */
+void mproc_yield(void) {
+    PCB prox_pcb,ant_pcb;
+
+    if (pcb_caller(&prox_pcb)>-1) {
+        insere_fifo(&fifo_aptos[executando.prio], executando);
+        ant_pcb    = executando;
+        executando = prox_pcb;
+        swapcontext(&ant_pcb.contexto,&executando.contexto);
+    }
+}
+
+/* Aguarda até que o processo identificado por "pid" seja encerrado.
+   Retorna "0" se funcionou corretamente ou "-1" caso contrário. */
 int mproc_join(int pid) {
     PCB prox_pcb,ant_pcb;
 
@@ -148,15 +169,7 @@ int mproc_join(int pid) {
     return 0;
 }
 
-
-void mproc_yield(void) {
-    PCB prox_pcb,ant_pcb;
-
-    if (pcb_caller(&prox_pcb)>-1) {
-        insere_fifo(&fifo_aptos[executando.prio], executando);
-        ant_pcb    = executando;
-        executando = prox_pcb;
-        swapcontext(&ant_pcb.contexto,&executando.contexto);
-    }
+/* Inicia o esclonador de processos, deve ser chamado apenas na função 
+   main() após as chamadas de mproc_create() */
+void scheduler(void) {
 }
-
